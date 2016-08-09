81eb96d45a82a1bbd2896c6f4cf56bc19a169ea8
Andre Miras <andre.miras@gmail.com>
2016-08-09 11:39:13 +0200

    Indentation fixes

--- lookup.py
+++ lookup.py
@@ -11,19 +11,19 @@ sock.bind( (UDP_IP,UDP_PORT) )
 mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 for host in ['esp01','microknoppix','pvknoppix','hprinter'][::-1]:
-#    the string in the following statement is an empty query packet
-     dns = dpkt.dns.DNS('\x00\x00\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x01')
-     dns.qd[0].name=host+'.local'
-     sock.sendto(dns.pack(),(MCAST_GRP,UDP_PORT))
+    # the string in the following statement is an empty query packet
+    dns = dpkt.dns.DNS('\x00\x00\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x01')
+    dns.qd[0].name=host+'.local'
+    sock.sendto(dns.pack(),(MCAST_GRP,UDP_PORT))
 sock.settimeout(5)
 while True:
-  try:
-     m=sock.recvfrom( 1024 );#print '%r'%m[0],m[1]
-     dns = dpkt.dns.DNS(m[0])
-     if len(dns.qd)>0:print dns.__repr__(),dns.qd[0].name
-     if len(dns.an)>0 and dns.an[0].type == dpkt.dns.DNS_A:print dns.__repr__(),dns.an[0].name,socket.inet_ntoa(dns.an[0].rdata)
-  except socket.timeout:
-     break
+    try:
+        m=sock.recvfrom( 1024 );#print '%r'%m[0],m[1]
+        dns = dpkt.dns.DNS(m[0])
+        if len(dns.qd)>0:print dns.__repr__(),dns.qd[0].name
+        if len(dns.an)>0 and dns.an[0].type == dpkt.dns.DNS_A:print dns.__repr__(),dns.an[0].name,socket.inet_ntoa(dns.an[0].rdata)
+    except socket.timeout:
+        break
 #DNS(qd=[Q(name='hprinter.local')]) hprinter.local
 #DNS(qd=[Q(name='pvknoppix.local')]) pvknoppix.local
 #DNS(qd=[Q(name='microknoppix.local')]) microknoppix.local
