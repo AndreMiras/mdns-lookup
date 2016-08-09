be9c7031bc6f7ade6a2f83e8e26d4ff7c8008ffd
Andre Miras <andre.miras@gmail.com>
2016-08-09 11:43:47 +0200

    various pep8 fixes

--- lookup.py
+++ lookup.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
-
+"""
+#DNS(qd=[Q(name='hprinter.local')]) hprinter.local
+#DNS(qd=[Q(name='pvknoppix.local')]) pvknoppix.local
+#DNS(qd=[Q(name='microknoppix.local')]) microknoppix.local
+#DNS(qd=[Q(name='esp01.local')]) esp01.local
+#DNS(an=[RR(name='esp01.local', rdata='\n\x00\x00\x04', ttl=120, cls=32769)], op=33792) esp01.local 10.0.0.4
+#DNS(an=[RR(name='PVknoppix.local', rdata='\n\x00\x00\xc2', ttl=120, cls=32769)], op=33792) PVknoppix.local 10.0.0.194
+"""
 import socket
 import struct
-import dpkt, dpkt.dns
+from dpkt.dns import DNS, DNS_A
 UDP_IP="0.0.0.0"
 UDP_PORT=5353
 MCAST_GRP = '224.0.0.251'
@@ -14,21 +21,18 @@ mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 for host in ['esp01','microknoppix','pvknoppix','hprinter'][::-1]:
     # the string in the following statement is an empty query packet
-    dns = dpkt.dns.DNS('\x00\x00\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x01')
+    dns = DNS('\x00\x00\x01\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x01')
     dns.qd[0].name=host+'.local'
-    sock.sendto(dns.pack(),(MCAST_GRP,UDP_PORT))
+    sock.sendto(dns.pack(), (MCAST_GRP, UDP_PORT))
 sock.settimeout(5)
 while True:
     try:
-        m=sock.recvfrom( 1024 );#print '%r'%m[0],m[1]
-        dns = dpkt.dns.DNS(m[0])
-        if len(dns.qd)>0:print dns.__repr__(),dns.qd[0].name
-        if len(dns.an)>0 and dns.an[0].type == dpkt.dns.DNS_A:print dns.__repr__(),dns.an[0].name,socket.inet_ntoa(dns.an[0].rdata)
+        m = sock.recvfrom(1024)
+        # print '%r'%m[0],m[1]
+        dns = DNS(m[0])
+        if len(dns.qd)>0:
+            print dns.__repr__(),dns.qd[0].name
+        if len(dns.an)>0 and dns.an[0].type == DNS_A:
+            print dns.__repr__(), dns.an[0].name, socket.inet_ntoa(dns.an[0].rdata)
     except socket.timeout:
         break
-#DNS(qd=[Q(name='hprinter.local')]) hprinter.local
-#DNS(qd=[Q(name='pvknoppix.local')]) pvknoppix.local
-#DNS(qd=[Q(name='microknoppix.local')]) microknoppix.local
-#DNS(qd=[Q(name='esp01.local')]) esp01.local
-#DNS(an=[RR(name='esp01.local', rdata='\n\x00\x00\x04', ttl=120, cls=32769)], op=33792) esp01.local 10.0.0.4
-#DNS(an=[RR(name='PVknoppix.local', rdata='\n\x00\x00\xc2', ttl=120, cls=32769)], op=33792) PVknoppix.local 10.0.0.194
