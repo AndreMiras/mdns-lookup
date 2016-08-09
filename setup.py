a699a25fc07f867505c1c8b61f34f73f6414d67b
Andre Miras <andre.miras@gmail.com>
2016-08-09 13:57:23 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mdns-lookup',
+      version='20160809',
+      description='Python mDNS lookup',
+      author='Andre Miras',
+      url='https://github.com/AndreMiras/mdns-lookup',
+      py_modules=['mdnslookup'],
+      scripts=['mdnslookup.py'],
+      install_requires=['dpkt'])
