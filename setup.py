039617f8501016a269141ffb0aadd61fe5cb34c4
Andre Miras <andre.miras@gmail.com>
2020-06-12 12:33:04 +0200

    :construction_worker: CI setup process
    Makes sure the setup always works

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
+import os
+from setuptools import setup
 
-from distutils.core import setup
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
 
 setup(name='mdns-lookup',
       version='20160930',
       description='Python mDNS lookup',
+      long_description=read("README.md"),
+      long_description_content_type="text/markdown",
       author='Andre Miras',
       url='https://github.com/AndreMiras/mdns-lookup',
       py_modules=['mdnslookup'],
       scripts=['mdnslookup/mdnslookup.py'],
+      setup_requires=['wheel'],
       install_requires=['dpkt'])
